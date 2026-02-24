d721e0dfcba984ded9ddf00ddcd2b3a7c1358540
aarmad <aaron.madjri@efrei.net>
2026-02-24 20:20:52 +0100

    Correction des liens d'images de la page Histoire

--- backend/models/data.js
+++ backend/models/data.js
@@ -3,55 +3,55 @@ exports.history = [
     year: 2011,
     event: 'Lancement de la Nintendo 3DS',
     details: 'Sortie mondiale avec la promesse d\'une expérience 3D sans lunettes. Le prix initial était de 249 $.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Nintendo-3DS-AquaOpen.jpg/1200px-Nintendo-3DS-AquaOpen.jpg'
+    image: 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Nintendo_3DS_Aqua_Blue_open.png'
   },
   {
     year: 2011,
     event: 'Succès de Super Mario 3D Land',
     details: 'Premier grand succès qui a démontré le potentiel de la 3D stéréoscopique pour le gameplay de précision.',
-    image: 'https://upload.wikimedia.org/wikipedia/en/1/12/Super_Mario_3D_Land.png'
+    image: 'https://m.media-amazon.com/images/I/910iW2E5pGL._AC_SL1500_.jpg'
   },
   {
     year: 2011,
     event: 'Mario Kart 7 révolutionne la série',
     details: 'Introduction du deltaplane et de la personnalisation des karts, devenant l\'un des jeux les plus vendus de la console.',
-    image: 'https://upload.wikimedia.org/wikipedia/en/f/f5/Mario_Kart_7.png'
+    image: 'https://m.media-amazon.com/images/I/910z7u0tL1L._AC_SL1500_.jpg'
   },
   {
     year: 2012,
     event: 'Lancement du modèle 3DS XL',
     details: 'Réponse aux demandes de plus grands écrans, offrant une immersion accrue avec une surface d\'affichage augmentée de 90 %.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Nintendo_3DS_XL_Red-Black.jpg/1200px-Nintendo_3DS_XL_Red-Black.jpg'
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Nintendo_3DS_XL_Red-Black.jpg'
   },
   {
     year: 2013,
     event: 'Légende de Zelda: A Link Between Worlds',
     details: 'Un chef-d\'œuvre revisitant le monde de A Link to the Past avec une mécanique de fusion murale innovante.',
-    image: 'https://upload.wikimedia.org/wikipedia/en/d/de/The_Legend_of_Zelda_A_Link_Between_Worlds_box_art.png'
+    image: 'https://m.media-amazon.com/images/I/81Pj6I0m4hL._AC_SL1500_.jpg'
   },
   {
     year: 2013,
     event: 'Arrivée de la Nintendo 2DS',
     details: 'Une version simplifiée et plus robuste sans effet 3D, ciblant un public plus jeune et un prix plus accessible.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nintendo-2DS-White-Red-Front.jpg/1200px-Nintendo-2DS-White-Red-Front.jpg'
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Nintendo-2DS-White-Red-Front.jpg'
   },
   {
     year: 2014,
     event: 'New Nintendo 3DS & XL',
     details: 'Une version "Pro" avec un second stick (C-Stick), des boutons ZL/ZR, un processeur plus puissant et une 3D stable.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/New-Nintendo-3DS-Black-Front.jpg/1200px-New-Nintendo-3DS-Black-Front.jpg'
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/36/New-Nintendo-3DS-Black-Front.jpg'
   },
   {
     year: 2020,
     event: 'La fin d\'une ère',
     details: 'Nintendo annonce l\'arrêt officiel de la production de la famille 3DS le 16 septembre 2020 après 9 ans de succès.',
-    image: 'https://img.gaming.gentside.com/article/1024/nintendo-3ds/la-production-de-la-nintendo-3ds-s-arrete_4a2c98d60c4100c5ad263435cc9690ade3234d7d.jpg'
+    image: 'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?q=80&w=2070&auto=format&fit=crop'
   },
   {
     year: 2023,
     event: 'Fermeture du Nintendo eShop',
     details: 'Le 27 mars 2023, il n\'est plus possible d\'acheter du contenu numérique, marquant le passage de la console au statut "rétro".',
-    image: 'https://cdn.pixabay.com/photo/2021/04/18/14/01/nintendo-6188445_1280.jpg'
+    image: 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070&auto=format&fit=crop'
   }
 ];
 
