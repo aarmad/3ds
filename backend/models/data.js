dc0ad41ef14a706c158eea8d6c60fdbcbdf8a476
aarmad <aaron.madjri@efrei.net>
2026-02-27 13:37:40 +0100

    Fix: Utilisation d'images provenant de romsfun.com pour la page histoire

--- backend/models/data.js
+++ backend/models/data.js
@@ -3,55 +3,55 @@ exports.history = [
     year: 2011,
     event: 'Lancement de la Nintendo 3DS',
     details: 'Sortie mondiale avec la promesse d\'une expérience 3D sans lunettes. Le prix initial était de 249 $.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Nintendo_3DS_Aqua_Blue_open.png'
+    image: 'https://romsfun.com/wp-content/uploads/2021/04/Nintendo_3ds_logo.png'
   },
   {
     year: 2011,
     event: 'Succès de Super Mario 3D Land',
     details: 'Premier grand succès qui a démontré le potentiel de la 3D stéréoscopique pour le gameplay de précision.',
-    image: 'https://m.media-amazon.com/images/I/910iW2E5pGL._AC_SL1500_.jpg'
+    image: 'https://romsfun.com/wp-content/uploads/2019/11/super-mario-3d-land-3ds-300x274.jpg'
   },
   {
     year: 2011,
     event: 'Mario Kart 7 révolutionne la série',
     details: 'Introduction du deltaplane et de la personnalisation des karts, devenant l\'un des jeux les plus vendus de la console.',
-    image: 'https://m.media-amazon.com/images/I/910z7u0tL1L._AC_SL1500_.jpg'
+    image: 'https://romsfun.com/wp-content/uploads/2019/11/mario-kart-7-3ds-300x274.jpg'
   },
   {
     year: 2012,
     event: 'Lancement du modèle 3DS XL',
     details: 'Réponse aux demandes de plus grands écrans, offrant une immersion accrue avec une surface d\'affichage augmentée de 90 %.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Nintendo_3DS_XL_Red-Black.jpg'
+    image: 'https://romsfun.com/wp-content/uploads/2021/04/Nintendo_3ds_logo.png'
   },
   {
     year: 2013,
-    event: 'Légende de Zelda: A Link Between Worlds',
-    details: 'Un chef-d\'œuvre revisitant le monde de A Link to the Past avec une mécanique de fusion murale innovante.',
-    image: 'https://m.media-amazon.com/images/I/81Pj6I0m4hL._AC_SL1500_.jpg'
+    event: 'Légende de Zelda: Ocarina of Time 3D',
+    details: 'Un chef-d\'œuvre classique revisité en 3D stéréoscopique.',
+    image: 'https://romsfun.com/wp-content/uploads/2019/11/Ocarina-of-Time-3D-3ds-300x267.jpg'
   },
   {
     year: 2013,
     event: 'Arrivée de la Nintendo 2DS',
     details: 'Une version simplifiée et plus robuste sans effet 3D, ciblant un public plus jeune et un prix plus accessible.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/a/ae/Nintendo-2DS-White-Red-Front.jpg'
+    image: 'https://romsfun.com/wp-content/uploads/2021/04/Nintendo_3ds_logo.png'
   },
   {
     year: 2014,
     event: 'New Nintendo 3DS & XL',
     details: 'Une version "Pro" avec un second stick (C-Stick), des boutons ZL/ZR, un processeur plus puissant et une 3D stable.',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/3/36/New-Nintendo-3DS-Black-Front.jpg'
+    image: 'https://romsfun.com/wp-content/uploads/2021/04/Nintendo_3ds_logo.png'
   },
   {
     year: 2020,
     event: 'La fin d\'une ère',
     details: 'Nintendo annonce l\'arrêt officiel de la production de la famille 3DS le 16 septembre 2020 après 9 ans de succès.',
-    image: 'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?q=80&w=2070&auto=format&fit=crop'
+    image: 'https://romsfun.com/wp-content/uploads/2021/04/Nintendo_3ds_logo.png'
   },
   {
     year: 2023,
     event: 'Fermeture du Nintendo eShop',
     details: 'Le 27 mars 2023, il n\'est plus possible d\'acheter du contenu numérique, marquant le passage de la console au statut "rétro".',
-    image: 'https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=2070&auto=format&fit=crop'
+    image: 'https://romsfun.com/wp-content/uploads/2021/04/Nintendo_3ds_logo.png'
   }
 ];
 
